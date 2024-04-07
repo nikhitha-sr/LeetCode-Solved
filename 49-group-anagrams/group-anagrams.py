5d0dead7c866a96d46ca5b2bf92435962629f30a
Nikhitha <159178571+nikhitha-sr@users.noreply.github.com>
2024-04-08 00:30:46 +0530

    Time: 98 ms (27.62%) | Memory: 20.2 MB (53.73%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.py
@@ -0,0 +1,12 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        output = []; anagram = {}; list_word =[]
+        for word in strs:
+            list_word = tuple(sorted(word))
+            if list_word in anagram:
+                anagram[list_word] = anagram[list_word] +[word]
+            else:
+                anagram[list_word] = [word]
+        for value in anagram.values():
+            output.append(value)
+        return output 
